c5e4f2fb930dd787ba9afc270db7066559e3005e
naom1029 <56006010+naom1029@users.noreply.github.com>
2026-07-06 02:41:52 +0900

    refactor(neovim): nvim設定をdotfilesへの直接シンボリンクに変更しプラグイン一括更新

--- nix/programs/neovim.nix
+++ nix/programs/neovim.nix
@@ -1,5 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 
+let
+  dotfilesDir = "${config.home.homeDirectory}/src/github.com/naom1029/dotfiles";
+  nvimDotfilesDir = "${dotfilesDir}/config/nvim";
+  nvimConfigDir = "${config.xdg.configHome}/nvim";
+in
 {
   programs.neovim = {
     enable = true;
@@ -8,8 +13,14 @@
     withPython3 = false;
   };
 
-  xdg.configFile."nvim" = {
-    source = ../../config/nvim;
-    recursive = true;
-  };
+  # home-manager の xdg.configFile ではなく dotfiles への直接シンボリンクにする
+  # lazy-lock.json 等を lazy.nvim が書き換えられるようにするため
+  xdg.configFile."nvim/init.lua".enable = lib.mkForce false;
+
+  home.activation.linkNvimConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [ -d "${nvimConfigDir}" ] && [ ! -L "${nvimConfigDir}" ]; then
+      rm -rf "${nvimConfigDir}"
+    fi
+    ln -sfn "${nvimDotfilesDir}" "${nvimConfigDir}"
+  '';
 }
