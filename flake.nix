09e1d48c07f17e8e224ff1f6854bc37b98b2dafe
naom1029 <56006010+naom1029@users.noreply.github.com>
2026-07-05 22:41:24 +0900

    feat: chezmoiからNix home-managerへ移行
    - flake.nixとhome.nixによる宣言的な設定管理を導入
    - git, delta, gh, fzf, tmux, lazygit, neovim, bash, readlineをprograms.*モジュールで管理
    - herdr設定を追加 (参考: wiki.adachin.me/archives/3355)
    - gitコミットにGitHub noreplyメールを使用
    - chezmoi管理のdotfilesを削除 (dot_bashrc, dot_gitconfig.tmpl等)
    - tmuxプラグインをtpmからhome-manager管理に移行

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "naom1029's home-manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      username = "naom1029";
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+}
