2658fa96c9afb22e814f12f1b129f8fa89c7c212
naom1029 <56006010+naom1029@users.noreply.github.com>
2026-07-20 16:55:59 +0900

    feat(conform): プロジェクトローカルのフォーマッタを優先（prefer_local）

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -26,6 +26,8 @@ return {
   },
   opts = {
     notify_on_error = false,
+    -- プロジェクトローカル（node_modules/.bin）のフォーマッタを PATH より優先
+    prefer_local = 'node_modules/.bin',
     format_on_save = {
       timeout_ms = 500,
       lsp_format = 'fallback',
