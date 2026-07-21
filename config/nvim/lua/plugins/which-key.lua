eece61c31e732d71faa0a64113ec4706f0ed85a0
naom1029 <56006010+naom1029@users.noreply.github.com>
2026-07-21 23:25:13 +0900

    revert(nvim): which-keyの自動トリガーを復元

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -6,9 +6,6 @@ return {
   event = 'VimEnter',
   opts = {
     delay = 0,
-    triggers = {
-      { '<leader>', mode = { 'n', 'x' } },
-    },
     icons = {
       mappings = vim.g.have_nerd_font,
       keys = vim.g.have_nerd_font and {} or {
